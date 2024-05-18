cb33f53f64cb8ca4857a1a62ebcc939f68733360
Lukas Veiga <lukas.veiga@hotmail.com>
2024-05-17 22:09:51 -0300

    feat: rename using camel case main project class entrypoint

--- hotel-reservation-system-api/src/main/java/com/devlukas/hotelreservationsystem/HotelReservationSystemApplication.java
+++ hotel-reservation-system-api/src/main/java/com/devlukas/hotelreservationsystem/HotelReservationSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelreservationsystemApplication {
+public class HotelReservationSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelreservationsystemApplication.class, args);
+		SpringApplication.run(HotelReservationSystemApplication.class, args);
 	}
 
 }
